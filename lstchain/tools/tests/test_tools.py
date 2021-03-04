6a313f30d92d0a207a4bbd0eae91175512ad9bea
Jose Enrique Ruiz <jer@iaa.es>
2021-03-04 18:05:34 +0100

    fix CI

--- lstchain/tools/tests/test_tools.py
+++ lstchain/tools/tests/test_tools.py
@@ -2,14 +2,13 @@ import pytest
 from pathlib import Path
 
 from ctapipe.core import run_tool
+from lstchain.tools.lstchain_create_drs4_pedestal_file import PedestalFITSWriter
+from lstchain.tests.test_lstchain import test_r0_path
 
 
 @pytest.mark.private_data
 def test_create_drs4_pedestal_file(temp_dir_observed_files):
-    from lstchain.tools.lstchain_create_drs4_pedestal_file import PedestalFITSWriter
-    from lstchain.tests.test_lstchain import test_r0_path
 
-    input_file = test_r0_path
     output_file = temp_dir_observed_files / "drs4_pedestal.fits"
     config = Path("./lstchain/data/onsite_camera_calibration_param.json").absolute()
 
@@ -18,10 +17,9 @@ def test_create_drs4_pedestal_file(temp_dir_observed_files):
             PedestalFITSWriter(),
             argv=[
                 f"--config={config}",
-                f"--input={input_file}",
+                f"--input={test_r0_path}",
                 f"--output={output_file}"
-            ],
-            cwd=temp_dir_observed_files,
+            ]
         )
         == 0
     )
